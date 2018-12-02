65e964d1f939cb6b6d6b7a009080cd7f49b293df
owdaa <awadodeh@gmail.com>
2018-12-01 18:06:15 -0600

    initial project

---
+++ src/main/java/com/ef/logbatch/LogBatchApplication.java
@@ -0,0 +1,12 @@
+package com.ef.logbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogBatchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ef/logbatch/LogBatchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ef.logbatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogBatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
